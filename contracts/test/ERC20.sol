b93e6624a8a6c0cd03a1725827b12dbfadf9a964
honestgoing <honestgoing@protonmail.com>
2021-11-25 20:56:25 +0800

    Change Pancake -> Yokai
    Cake -> YOK

--- contracts/test/ERC20.sol
+++ contracts/test/ERC20.sol
@@ -1,8 +1,8 @@
 pragma solidity =0.5.16;
 
-import '../PancakeERC20.sol';
+import '../YokaiERC20.sol';
 
-contract ERC20 is PancakeERC20 {
+contract ERC20 is YokaiERC20 {
     constructor(uint _totalSupply) public {
         _mint(msg.sender, _totalSupply);
     }
